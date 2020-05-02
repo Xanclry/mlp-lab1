e4957c6b8846559642134ad0e84787c405dac130
Your Name <fed_rekish@hotmail.com>
2020-05-02 20:40:01 +0300

    Added pop-up window on buying section

--- js/src/hamburger.js
+++ js/src/hamburger.js
@@ -1,17 +1,17 @@
-let toggleHamburger = function() {
-    if( $("body").css('overflow') !== 'hidden') {
+let toggleHamburger = function () {
+    if ($("body").css('overflow') !== 'hidden') {
         $("body").css("overflow", "hidden");
 
     } else {
         $("body").css("overflow", "unset");
     }
-    $(".dark-wrap").toggleClass("active")
-    $(".hamburger-menu").toggleClass("active")
-    $(".hamburger-menu__icon").toggleClass("active")
+    $(".dark-wrap").toggleClass("active");
+    $(".hamburger-menu").toggleClass("active");
+    $(".hamburger-menu__icon").toggleClass("active");
     $(".hamburger-menu_content").toggleClass("active")
-}
+};
 
-$(".hamburger-menu__icon").click(toggleHamburger)
+$(".hamburger-menu__icon").click(toggleHamburger);
 
-$(".menu__item").click(toggleHamburger)
+$(".menu__item").click(toggleHamburger);
 
