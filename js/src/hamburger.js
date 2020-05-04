20f0bfc13173098774085247c63db730d66a61b0
Xanclry <38082705+Xanclry@users.noreply.github.com>
2020-05-04 14:57:05 +0300

    Lab 2-5 (#3)
    * Added basic markup without any pictures
    
    * Added 'article' and 'section' tags, added some icons and pictures.
    
    * Layout of box-grid is done. Added some new backgrounds and icons. Adjusted fonts colors.
    
    * 'Four columns' and 'Full image' sections are done
    
    * 'Colour' section is done. Fixed some fonts. Added support of big resolutions.
    
    * 'Buying' and footer sections is done.
    
    * Fixed minor layout errors
    
    * Added grid layout to box-grid
    
    * Fixed minor issues and added fonts.css file
    
    * added basic adaptive design
    
    * added mobile version of the site
    
    * code refactoring
    
    * added side menu for mobile version
    
    * Added pop-up window on buying section
    
    * fix: anchor links in desktop version

---
+++ js/src/hamburger.js
@@ -0,0 +1,22 @@
+let toggleHamburger = function () {
+    if ($("body").css('overflow') !== 'hidden') {
+        $("body").css("overflow", "hidden");
+
+    } else {
+        $("body").css("overflow", "unset");
+    }
+    $(".dark-wrap").toggleClass("active");
+    $(".hamburger-menu").toggleClass("active");
+    $(".hamburger-menu__icon").toggleClass("active");
+    $(".hamburger-menu_content").toggleClass("active")
+};
+
+$(".hamburger-menu__icon").click(toggleHamburger);
+
+$(".menu__item").click(function () {
+    if ($(".hamburger-menu").hasClass('active')) {
+        toggleHamburger()
+    }
+});
+
+
